8bf262e1bcbf46db29625b640821433249c2f499
VitorAugusto966 <69120777+VitorAugusto966@users.noreply.github.com>
2022-09-07 21:50:44 -0300

    Add files via upload

---
+++ connection.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize-typescript";
+
+
+import { Acesso} from "./models/acesso"
+import { Estacao } from "./models/estacao";
+import { User } from "./models/usuario";
+const connection = new Sequelize({
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "",
+  database: "teste",
+  logging: false,
+  models: [ User,Estacao,Acesso],
+});
+
+export default connection;
