62334b03d47f3c73f88916a428e1a21fdd9cc09b
VitorAugusto966 <augusto.vitor@aluno.ifsp.edu.br>
2022-09-10 20:44:53 -0300

    Alterando a classe usuário

---
+++ models/usuario.ts
@@ -0,0 +1,46 @@
+import { Table, Model, Column, DataType, BelongsToMany, PrimaryKey, ForeignKey } from "sequelize-typescript";
+import { Acesso } from "./acesso";
+import { Estacao } from "./estacao";
+
+/// Adicionar tipo - status
+
+
+@Table({
+  timestamps: false,
+  tableName: "usuario",
+  
+})
+export class User extends Model {
+  @BelongsToMany(() => Estacao, () => Acesso)
+  estacoes!: Estacao[];
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  email!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  senha!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  nome!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  tipo!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  status!: string;
+}
